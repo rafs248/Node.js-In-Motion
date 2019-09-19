d907e9546eddf176964dc84d7c19f979b5a72f91
rafs248@gmail.com <drzewko60Koc>
2019-09-19 17:47:42 +0100

    EX3 - completed

--- Unit 2 - Nodejs On The Command Line/U2-X3-promisify-get/promisify-get.js
+++ Unit 2 - Nodejs On The Command Line/U2-X3-promisify-get/promisify-get.js
@@ -1,2 +1,27 @@
+/**
+ * Promisify reguest.get() function and use it to download few pictures from public bucket in AWS S3.
+ */
+
 // TODO: Continue on this program
 console.log("Getting files form S3 ...");
+
+
+const req = require('request');
+
+
+function getFilePromise(uri){
+    return new Promise(((resolve, reject) => {
+        req.get({uri:uri, json:true}, function (error, response, body) {
+            if(error!=null){
+                reject(error);
+            }else{
+                resolve(body);
+            }
+        })
+    }));
+}
+
+getFilePromise("https://rafs248-pub.s3.eu-west-2.amazonaws.com/ec2-data.json")
+    .then(body=>console.log("Retrieved with promise: ",body[2].age))
+    .catch(error=>console.log("Promise error: ",error));
+
