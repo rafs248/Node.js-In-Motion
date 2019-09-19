4b964440a2abea830e29add55e32b6dc0b1ccd53
rafs248@gmail.com <drzewko60Koc>
2019-09-19 08:19:20 +0100

    Ex2 - completed

--- Unit 2 - Nodejs On The Command Line/U2-X2-ec2-status/ec2-status.js
+++ Unit 2 - Nodejs On The Command Line/U2-X2-ec2-status/ec2-status.js
@@ -1,15 +1,10 @@
 // TODO: continue on this program
 
 /**
- * SPEC: This program will display list of EC2 instances in AWS including status: running / stoppped.
+ * SPEC: This program will download json file from S3 and list few fields from it.
  *
- * STATUS: does not work: Request path contains unescaped characters.
+ * STATUS: done
  *
- *
- * PLAN:
- * 1.Make a request to bbc and just display whatever returned (done)
- * 2.Prepare API request to AWS - can be in postman and use it in program
- * 3.Display specific fields from AWS response
  *
  */
 
@@ -18,19 +13,14 @@ console.log('Checking EC2 machines ...');
 
 const req = require('request');
 
-// // There is only one event to handle - when we get full response
-// req.get({uri: 'https://rafs248-pub.s3.eu-west-2.amazonaws.com/www-test-file-2.txt', json: false, proxy:'http://proxy-l.com.hmpo.net%3A3128'}, (error, response, body) => {
-//     console.log(`Received error: ${error}`);
-//     console.log(`Received response:`,response);
-// //    console.log(`Received body: ${body}`);
-//     });
-
 var proxyUrl = "http://proxy-l.com.hmpo.net%3A3128";
 
 var proxiedRequest = req.defaults({'proxy': proxyUrl});
 
-req.get("https://rafs248-pub.s3.eu-west-2.amazonaws.com/www-test-file-2.txt", function (error, response, body) {
-    console.log(`Received error: ${error}`);
-    console.log(`Received response:`,response);
+req.get({uri:"https://rafs248-pub.s3.eu-west-2.amazonaws.com/ec2-data.json", json:true}, function (error, response, body) {
     console.log(`Received body: ${body}`);
+    for(m in body){
+        console.log(`Machine ${body[m].name} is ${body[m].status}`);
+    }
+    console.log(`Received error: ${error}`);
 })
