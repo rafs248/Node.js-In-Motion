7fe578a6444a4f82346706f4c119c28a7268d419
rafs248@gmail.com <drzewko60Koc>
2019-10-04 18:00:11 +0100

    Added chuck file

--- Unit 3 - Building Web Applications With Node/U3-EX1-M38/index.js
+++ Unit 3 - Building Web Applications With Node/U3-EX1-M38/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const chuck_module = require('./chuck');
 
 const app = express();
 
 app.get('/',( req,resp) => {
     let myTemplate = 'index';
-    let data = {currentTime: new Date()};
-    resp.render(`${__dirname}/templates/${myTemplate}.pug`, data );
-    resp.end();
+    chuck_module.getChuckNorrisFact(msg=>{
+        let data = {currentTime: new Date(), chuckMessage:msg};
+        resp.render(`${__dirname}/templates/${myTemplate}.pug`, data );
+        resp.end();
+    });
+
 }).listen(8080);
 
 console.log("Server listening on 8080");
