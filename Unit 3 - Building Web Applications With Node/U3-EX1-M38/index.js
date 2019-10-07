21a4ee16ee90c336bb86ea83442981ca6ee532f8
Rafal <rafal-wojciech.surowiec@digital.homeoffice.gov.uk>
2019-10-07 14:18:59 +0100

    Changed to promise

--- Unit 3 - Building Web Applications With Node/U3-EX1-M38/index.js
+++ Unit 3 - Building Web Applications With Node/U3-EX1-M38/index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const chuck_module = require('./chuck');
 
 const app = express();
+let port = 8081;
 
-app.get('/',( req,resp) => {
+
+
+app.get('/', (req,resp) => {
     let myTemplate = 'index';
-    chuck_module.getChuckNorrisFact(msg=>{
-        let data = {currentTime: new Date(), chuckMessage:msg};
+
+    (async()=>{
+        const data = await getChuckNorrisFactPromise();
         resp.render(`${__dirname}/templates/${myTemplate}.pug`, data );
         resp.end();
-    });
+    })();
 
 });
 
@@ -18,7 +22,20 @@ app.get('/style.css', (req, res) => {
     res.sendFile(__dirname + '/style.css');
 });
 
-app.listen(8080);
+app.listen(port);
 
 
-console.log("Server listening on 8080");
+console.log("Server listening on ",port);
+
+getChuckNorrisFactPromise = () =>{
+
+    console.log("Now using await");
+    const promise = new Promise((resolve)=>{
+        chuck_module.getChuckNorrisFact(msg=>{
+            let data = {currentTime: new Date(), chuckMessage:msg};
+            resolve(data);
+        });
+
+    });
+    return promise;
+}
